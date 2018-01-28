1a1e46fa945567a39688b56bd27937166698d140
Quadarius Moore <qkmoore@crimson.ua.edu>
2018-01-28 02:51:44 -0600

    Various additions and improvements
    Fixed start time
    Colors now change depending on time of day

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -1,5 +1,11 @@
 var rocky = require('rocky');
 
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
 rocky.on('draw', function(event) {
   // Get the CanvasRenderingContext2D object
   var ctx = event.context;
@@ -12,33 +18,48 @@ rocky.on('draw', function(event) {
   var h = ctx.canvas.unobstructedHeight;
 
   // Current date/time
-  var m = new Date();
-  var m2 = m.setHours(0,0,0,0);
-  var last_midnight = Math.round( m2 / 1000 );
   var d = new Date();
-  var now_time = Math.round(Date.now() / 1000);
-  var print_time = now_time - last_midnight;
+  var uts_clock_start = Math.round( d.setHours(8,0,0,0) / 1000 );
+  var uts_clock_now = Math.round(Date.now() / 1000);
+  var clock_time = uts_clock_now - uts_clock_start;
+  
+  if( clock_time < 0 ) { 
+    clock_time += 86400;
+  }
+  
   // Set the text color
-  ctx.fillStyle = 'white';
+  if ( clock_time >= 0 && clock_time < 32400 ) {
+    ctx.beginPath();
+    ctx.rect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+    ctx.fillStyle = '#e70012';
+    ctx.fill();
+    ctx.fillStyle = 'white';
+  } else if ( clock_time >= 32400 && clock_time < 54000 ) {
+    ctx.beginPath();
+    ctx.rect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+    ctx.fillStyle = 'green';
+    ctx.fill();
+    ctx.fillStyle = '#123524';
+  } else {
+    ctx.beginPath();
+    ctx.rect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+    ctx.fillStyle = '#191970';
+    ctx.fill();
+    ctx.fillStyle = 'white';
+  }
+  
 
   // Center align the text
   ctx.textAlign = 'center';
+  ctx.font = '42px bold numbers Leco-numbers';
   
-  if (rocky.userPreferences.contentSize === 'x-large') {
-    ctx.font = '42px bold numbers Leco-numbers';
-  } else {
-    ctx.font = '32px bold numbers Leco-numbers';
-  }
   
   // Display the time, in the middle of the screen
-  ctx.fillText( print_time, w / 2, h / 3 );
-//   ctx.fillText( d.toLocaleTimeString(). w / 2, h / 2);
+  ctx.fillText( pad( clock_time, 5 ), w / 2, h / 3 );
+
 });
 
 rocky.on('secondchange', function(event) {
-  // Display a message in the system logs
-  console.log("Another minute with your Pebble!");
-
   // Request the screen to be redrawn on next pass
   rocky.requestDraw();
 });
